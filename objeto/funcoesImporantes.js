b7f6a92b73d2546fc6eebf467d1bb55cfd3bc5d1
Filipe Regis <filipe.regis@datainfo.inf.br>
2019-03-21 11:15:31 -0300

    Commit 5

---
+++ objeto/funcoesImporantes.js
@@ -0,0 +1,24 @@
+const pessoa = {
+    nome : 'Rebeca',
+    idade : 15,
+    peso : 50
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+
+Object.entries(pessoa).forEach(e => {
+    console.log(`${e[0]}: ${e[1]}`)
+});
+//Mesma coisa
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`)
+});
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true,
+    writable: false,
+    value: '01/01/2003'
+})
