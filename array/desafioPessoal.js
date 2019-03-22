b2964c0401b3f2f8422b6dd6259cb11cc81f3700
Filipe Regis <filipe.regis@datainfo.inf.br>
2019-03-22 10:23:56 -0300

    Commit array

--- array/desafioPessoal.js
+++ array/desafioPessoal.js
@@ -1,14 +1,13 @@
-const telefones = [991915007, 30371779, 992726215, 984224210]
+const telefones = [991915007, 30371779, 992726215, 984224210, 'abcdefghjiadssad']
 const mascaraTelefone = (e) => {
-    const a = {telefone: e};
-    console.log(a.telefone)
-    for(let n in a){
-        console.log(n)
+    const c = new String(e);
+    if (c.length == 9) {
+        return `(47) ${c.substring(0, 5)}-${c.substring(5)}`
+    } else if (c.length == 8) {
+        return `(47) ${c.substring(0, 4)}-${c.substring(4)}`
+    } else {
+        return c.toLocaleUpperCase();
     }
-    if(a.length > 4){
-        console.log('a')
-    }
-    return `(47) ${e}`
 }
 
 console.log(telefones.map(mascaraTelefone))
--- array/map1.js
+++ array/map1.js
@@ -1,12 +1,17 @@
-const nums = [1, 2, 10, 4, 5]
+const nums = [1, 2, 3, 4, 5]
 
 //For com proposito
 let resultado = nums.map(function (e) {
-    return `${e * 3}/${e * 2}/${(2000 + (e + 20) / e)}`;
+    return `${e * 3}/${e * 2}/${(2000 + (e + 20) / e).toFixed(0)}`;
 })
 
 console.log(resultado)
 
 const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
 
-console.log(nums.map(soma10))
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+
+console.log(resultado[0])
+
